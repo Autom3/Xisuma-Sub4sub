51dbb48b2dcac090e7b7e6786f2c6568af48d6ba
Zapdos26 <zapdoskid@gmail.com>
2020-06-10 20:00:43 -0400

    Documentation

--- src/commands/selfpromo.ts
+++ src/commands/selfpromo.ts
@@ -6,16 +6,24 @@ dotenv.config();
 
 const CHANNEL_ID = process.env.CHANNEL_ID;
 
+/**
+ * @description Function to be run when the user performs the selfpromo command
+ * @param {Message} message - Discord message sent by user
+ * @param args - Arguments for the command
+ * @param userCache - Cache of users who've used the commands in the past. User's entry expires after certain amount of time
+ */
 export const selfpromo = (
   message: Message,
   args: string[],
   userCache: NodeCache
 ): void => {
+  //Checks to see if the user is still on cooldown
   if (userCache.has(message.author.id)) {
     message.reply("You're still on cooldown.");
     message.delete();
     return;
   }
+  //Checks to see if the link is valid and sends message.
   if (
     /https:\/\/(www\.)?((twitch\.tv\/[a-zA-Z0-9_]+)|(youtube\.com\/watch\?v=[a-zA-Z\-\_0-9]{11})|(youtu\.be\/[a-zA-Z\-\_0-9]{11})|(mixer\.com\/[a-zA-Z0-9]+)|(behance\.net\/gallery\/\d*?\/[0-9a-zA-Z\-]+?))/.test(
       args[0]
@@ -26,8 +34,9 @@ export const selfpromo = (
     channel.send(`${message.author.tag}: ${args[0]}`);
     userCache.set(message.author.id, true);
     message.delete();
-  } else {
-    console.log(args[0]);
+  } //If link is not valid, tell user that link is not valid
+  else {
+    console.log("Failed with arguments of:" + args[0]);
     message.reply("You are not allowed to send that");
     message.delete();
   }
--- src/server.ts
+++ src/server.ts
@@ -5,19 +5,25 @@ import { selfpromo } from "./commands/selfpromo";
 
 dotenv.config();
 
+//Sets up global variables
 const ACCESS_TOKEN = process.env.DISCORD_BOT_ACCESS_TOKEN;
 const PREFIX = process.env.PREFIX || ":";
 const USER_COOLDOWN = parseInt(process.env.USER_COOLDOWN || "1440");
 
+//Sets up Discord bot client
 const bot = new Client();
+
+//Sets up cooldown cache
 const userCache = new NodeCache({ stdTTL: USER_COOLDOWN * 60 });
 
+//Verifies that there is a valid access token
 if (ACCESS_TOKEN == null) {
   console.log("No Access Token Supplied");
 } else {
   bot.login(ACCESS_TOKEN);
 }
 
+//Initial setup once bot logs in
 bot.on("ready", () => {
   console.log("Logged on as " + bot.user.tag);
   bot.user.setPresence({
@@ -26,6 +32,7 @@ bot.on("ready", () => {
   });
 });
 
+//Checks message to see if there are commands, and responds accordingly
 bot.on("message", (message) => {
   if (!message.content.startsWith(PREFIX) || message.author.bot) return;
   if (
@@ -33,14 +40,12 @@ bot.on("message", (message) => {
     typeof message.guild === undefined
   ) {
     message.reply(
-      "This bot commands can only be executed from the Discord server channel!"
+      "Bot commands can only be executed from the Discord server channel!"
     );
     return;
   }
-
   const args = message.content.slice(PREFIX.length).split(/ +/);
   const command = args.shift().toLowerCase();
-
   if (command == "selfpromo") {
     selfpromo(message, args, userCache);
   }
